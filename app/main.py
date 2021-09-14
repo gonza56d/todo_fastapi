9d600de3e34a0594e2fb0e5aecfee8281eae7738
gonza <gonza56d@gmail.com>
2021-09-14 03:15:16 -0300

    FastAPI MongoDB connection;

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 """Project configurations."""
 
-# Python
-from typing import Optional
-# FastAPI
 from fastapi import FastAPI
+from motor import motor_asyncio
 
 
 # FastAPI instance
 app = FastAPI()
+
+# MongoDB Connection
+client = motor_asyncio.AsyncIOMotorClient('mongodb://root:example@mongo:27017/')
+db = client.college
 
 
 @app.get('/')
